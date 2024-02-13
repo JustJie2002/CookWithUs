9844215bcf0c647812c544267098c9bf9c5eb6a5
Jie Chen <jiechen067@gmail.com>
2024-02-13 18:57:48 -0500

    Got website working

--- src/app/views.py
+++ src/app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<p1>Welcome To CookWithUs<p1>")
+    return render(request, "home.html")
